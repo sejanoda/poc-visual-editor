41ef88a71bb87d4e024471d5f68a5242987ee214
srgesio <gesioiajm@gmail.com>
2024-02-17 12:25:14 -0300

    feat: setting a editor route

---
+++ app/allPages.tsx
@@ -0,0 +1,9 @@
+import Home from "./page";
+import Test from "./test/page";
+
+export default function allPages() {
+    return {
+        home: Home,
+        test: Test
+    }
+}
---
+++ app/test/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Test() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-red-500">
+
+        </main>
+    );
+}
